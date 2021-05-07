dcecef62855eae875db0d41b78fe29887f280b0e
Dimitar <dimemilkov1@gmail.com>
2021-05-07 18:37:12 +0200

    my commit

---
+++ domasna1.js
@@ -0,0 +1,49 @@
+let name = 'Dimitar';
+let surname = 'Milkov';
+let age = 23;
+let isStudent = true;
+let isWorking = false;  
+// primitive type
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(isStudent);
+console.log(isWorking);
+
+console.log(typeof name);
+console.log(typeof surname);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof isStudent);
+console.log(typeof isWorking);
+
+
+const dun = 5.15;
+// const dun = 6.18; when you change the constant it says we make a SyntaxError because const dun already has been declared.
+
+let student1 = {
+    fName = 'Vasil' ,
+    lName = 'Vasilev' ,
+    age = 23
+}
+
+let student2 = {
+    fName = 'Darko' ,
+    lName = 'Petrov' ,
+    age = 23
+}
+
+let students = [student1, student2]
+
+console.log(student1);
+console.log(student2);
+console.log(students);
+
+console.log(typeof student1);
+console.log(typeof student2);
+console.log(typeof students);
+
+
+//What is the type of array variables?
+//-An array is a special type of variable, which can store multiple values using special syntax.
