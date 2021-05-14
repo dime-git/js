b291bc77fcf1e8c5e0917bc59265d93b6eeea86b
dime-git <80923041+dime-git@users.noreply.github.com>
2021-05-14 18:40:11 +0200

    Add files via upload

---
+++ domasna3.js
@@ -0,0 +1,44 @@
+/* 1. Declare variables and use the assignment operators ( print the results) */
+
+let d = 12;
+let m = 16;
+let j = 20;
+let t = 26;
+let v = 38;
+
+d += 16;
+console.log('d=', d);
+
+m -= 12;
+console.log('m=', m);
+
+j *= -13;
+console.log('j=', j);
+
+t /= 7;
+console.log("t=", t);
+
+v %= 4;
+console.log('v=', v);
+
+
+//2. Write a JavaScript conditional statement to sort three numbers. Print the result.
+
+let x = 128;
+let y = 77; 
+let z = 212;
+
+if (x > y && z > y) {
+
+    if (y < x){
+    console.log('From biggest so smallest', z, x, y)
+    } else (x > z) 
+        console.log('From smallest to biggest', y, x, z)
+    };
+    
+    
+//3. Compute the average marks of the following students. Then, use the average to determine the corresponding grade.   
+//4. For a given day, print out whether it is a day of the work week or weekend.
+//5. For a given month, print out in which season it belongs.
+
+   
