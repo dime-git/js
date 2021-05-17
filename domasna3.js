7590917ebc9ef393622644d40aa497afbe9ab50a
dime-git <80923041+dime-git@users.noreply.github.com>
2021-05-17 20:51:30 +0200

    Add files via upload

--- domasna3.js
+++ domasna3.js
@@ -38,7 +38,90 @@ if (x > y && z > y) {
     
     
 //3. Compute the average marks of the following students. Then, use the average to determine the corresponding grade.   
+let average = (80 + 77 + 88 + 95 + 68) / 5 ;
+
+if (average < 60) {
+    console.log('Grade F');
+} else if (average < 70) {
+    console.log('Grade D');
+} else if (average < 80) {
+    console.log('Grade C');
+} else if (average < 90) {
+    console.log('Grade B');
+} else if (average < 100) {
+    console.log('Grade A')
+} ;
+
 //4. For a given day, print out whether it is a day of the work week or weekend.
+
+let day = 'Tuesday';
+
+switch(day) {
+    case 'Monday':
+        console.log('Week day');
+        break;
+        case 'Tuesday':
+            console.log('Week day') ;
+            break;
+        case 'Wednesday':
+            console.log('Week day');
+            break;
+        case 'Thursday':
+            console.log('Week day');
+        break;
+        case 'Friday':
+            console.log('Week day');
+            break;
+        case 'Saturday':
+            console.log('Weekend');
+            break;
+        case 'Sunday':
+            console.log('Weekend');
+            break;
+}; 
+
+
+
 //5. For a given month, print out in which season it belongs.
 
-   
+let month = 'July' ;
+
+
+switch(month) {
+    case 'January': 
+        console.log('winter');
+        break;
+    case 'February' :
+        console.log('winter');
+        break;
+    case 'March':
+        console.log('spring');
+        break;
+    case 'April':
+        console.log('spring');
+        break;
+    case 'May':
+        console.log('spring');
+        break;
+    case 'June':
+        console.log('summer');
+        break;
+    case 'July':
+        console.log('summer');
+        break;
+    case 'August':
+        console.log('summer');
+        break;
+    case 'September':
+        console.log('autmn');
+        break;
+    case 'October':
+        console.log('autumn');
+        break;
+    case 'November':
+        console.log('autumn');
+        break;
+    case 'December':
+        console.log('winter');
+        break;
+};
